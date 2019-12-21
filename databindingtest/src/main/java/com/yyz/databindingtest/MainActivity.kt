19e86ea8dd1f6924e96dbf2bdb535b5ad1a08fae
Young Yunzhao <fly_with24@163.com>
2019-12-21 11:17:19 +0800

    使用DataBinding 实现点赞取消赞

--- databindingtest/src/main/java/com/yyz/databindingtest/MainActivity.kt
+++ databindingtest/src/main/java/com/yyz/databindingtest/MainActivity.kt
@@ -2,11 +2,19 @@ package com.yyz.databindingtest
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProviders
+import com.yyz.databindingtest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mLiveDataViewModel: LiveDataViewModel
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        mLiveDataViewModel = ViewModelProviders.of(this).get(LiveDataViewModel::class.java)
+        binding.result = mLiveDataViewModel
+        binding.lifecycleOwner = this
     }
 }
