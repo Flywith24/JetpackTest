4499310a4438a2d7fb38fa28fe6a5e503d43f553
fly_with24 <fly_with24@163.com>
2019-12-24 16:22:09 +0800

    finish jump to page

--- demo_navigation/src/main/java/com/yyz/demo_navigation/DetailFragment.kt
+++ demo_navigation/src/main/java/com/yyz/demo_navigation/DetailFragment.kt
@@ -5,7 +5,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 
 /**
  * A simple [Fragment] subclass.
@@ -20,5 +22,10 @@ class DetailFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_detail, container, false)
     }
 
-
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        view?.findViewById<Button>(R.id.button)?.setOnClickListener {
+            Navigation.findNavController(it).navigate(R.id.action_detailFragment_to_homeFragment)
+        }
+    }
 }
--- demo_navigation/src/main/java/com/yyz/demo_navigation/HomeFragment.kt
+++ demo_navigation/src/main/java/com/yyz/demo_navigation/HomeFragment.kt
@@ -5,7 +5,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 
 /**
  * A simple [Fragment] subclass.
@@ -20,5 +22,10 @@ class HomeFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        view?.findViewById<Button>(R.id.button)
+            ?.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_homeFragment_to_detailFragment))
+    }
 
 }
