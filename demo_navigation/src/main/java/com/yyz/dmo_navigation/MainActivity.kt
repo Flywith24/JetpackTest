6afc0e9f71085fa99c39d3977b4b56c16f6b496d
fly_with24 <fly_with24@163.com>
2019-12-24 15:10:30 +0800

    add navigation

---
+++ demo_navigation/src/main/java/com/yyz/dmo_navigation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.yyz.dmo_navigation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
