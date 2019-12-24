b28137cfbba4e96d90b455076e0cf64c78392189
fly_with24 <fly_with24@163.com>
2019-12-24 16:30:40 +0800

    add back button

--- demo_navigation/src/main/java/com/yyz/demo_navigation/MainActivity.kt
+++ demo_navigation/src/main/java/com/yyz/demo_navigation/MainActivity.kt
@@ -2,11 +2,18 @@ package com.yyz.demo_navigation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val controller = Navigation.findNavController(this, R.id.fragment)
+        NavigationUI.setupActionBarWithNavController(this, controller)
     }
+
+    override fun onSupportNavigateUp(): Boolean =
+        Navigation.findNavController(this, R.id.fragment).navigateUp()
 }
